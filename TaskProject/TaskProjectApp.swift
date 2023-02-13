6dfde2deb0bc47cd56f91038d952fe7563f2f4b4
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-02-13 14:58:08 +0000

    Initial Commit

---
+++ TaskProject/TaskProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskProjectApp.swift
+//  TaskProject
+//
+//  Created by Abdoulaye Aliou SALL on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
